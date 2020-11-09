73b8755125d7f6a7201dfb1beb03e1abdb1952bf
Cosmin392 <cosminanghel7117@gmail.com>
2020-11-09 11:23:47 +0100

    Aggiungere i file di progetto.

---
+++ Dimensione dell'immaggine/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Dimensione_dell_immaggine
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Inserisci il primo valore");
+            int n1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Inserisci il secondo valore");
+            int n2 = Convert.ToInt32(Console.ReadLine());
+            int calcolo = ((n1 * n2) * 3) / 1024;
+                Console.WriteLine($"L'immaggine pesa {calcolo}KB");
+            //co
+        }
+    }
+}
